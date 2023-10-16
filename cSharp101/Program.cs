bef9a14be75f64d0663dc6be0efc985bca413288
onurkaygn <92956138+onurkaygn@users.noreply.github.com>
2023-10-16 15:59:26 +0300

    karar yapilari eklendi

--- cSharp101/Program.cs
+++ cSharp101/Program.cs
@@ -6,7 +6,7 @@ namespace cSharp101
     {
         static void Main(string[] args)
         {
-            //Değişkenler ve Veri Tiplerş
+            //Değişkenler ve Veri Tipleri
             int year = 1955;
             string name = "Bill";
             String surname = "Gates";
@@ -22,25 +22,66 @@ namespace cSharp101
             int numberInt = 28;
             double numberDouble;
             numberDouble = numberInt;
-            //Try Catch Finally ve Mantıksal Hatalar
+            //Hata Yönetimi ve Kod İzleme
             try
             {
-                Console.WriteLine("What is your name?");
+                Console.WriteLine("What is your favorite number?");
                 int input = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Your name is: " + input);
+                Console.WriteLine("Your favorite number is: " + input);
             }
             catch(Exception ex) {
                 Console.WriteLine("Your exception is: " + ex.Message.ToString());
 
             }
-           
             finally
             {
-                Console.WriteLine("Done!");
+                Console.WriteLine("");
+            }
+            //Karar Yapıları
+            int newDate = DateTime.Today.Year;
+            if(newDate == 2023)
+            {
+                Console.WriteLine("It's 2023!");
+            }
+            else
+            {
+                Console.WriteLine("It's not 2023!");
             }
 
+            Console.WriteLine("Enter a number between 1 and 5...");
+            int yourNumberInput = Convert.ToInt32(Console.ReadLine());
           
+          
+           
+                switch(yourNumberInput)
+                {
+                    case 1:
+                        Console.WriteLine("Your number is: " + yourNumberInput); 
+                        break;
+                    case 2:
+                        Console.WriteLine("Your number is: " + yourNumberInput);
+                        break;
+                    case 3:
+                        Console.WriteLine("Your number is: " + yourNumberInput);
+                        break;
+                    case 4:
+                        Console.WriteLine("Your number is: " + yourNumberInput);
+                        break;
+                    case 5:
+                        Console.WriteLine("Your number is: " + yourNumberInput);
+                        break;
+                        default:
+                    Console.WriteLine( yourNumberInput + " is not a valid number.");
+                    break;
+                }
+
+            }
 
         }
+
+
+
+
+
     }
-}
+    
