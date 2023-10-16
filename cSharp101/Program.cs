deb838db2caf68456a0e201f7536fa55d0403bbd
onurkaygn <92956138+onurkaygn@users.noreply.github.com>
2023-10-16 15:07:38 +0300

    tip dönüşümleri eklendi

--- cSharp101/Program.cs
+++ cSharp101/Program.cs
@@ -6,6 +6,7 @@ namespace cSharp101
     {
         static void Main(string[] args)
         {
+            //Değişkenler ve Veri Tiplerş
             int year = 1955;
             string name = "Bill";
             String surname = "Gates";
@@ -13,6 +14,15 @@ namespace cSharp101
             DateTime today = DateTime.Now;
             Console.WriteLine("I am " + name + surname + " and my year of birth is " + year + " so my age is " + age + ".");
             Console.WriteLine("Wow today is " + today);
+            //Operatörler
+            int x = 5;
+            bool booleanX = x == 4;
+            Console.WriteLine(booleanX);
+            //Tip Dönüşümleri
+            int numberInt = 28;
+            double numberDouble;
+            numberDouble = numberInt;
+            
 
         }
     }
